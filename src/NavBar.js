1908154a8a8bdedd03d74937fee2baa04f1f7244
Alex Van Vleet <alexvan73@yahoo.com>
2022-12-06 16:36:14 -0700

    updated navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,12 +3,9 @@ import Button from "@mui/material/Button";
 
 export default function Navbar() {
   return (
-    <div
-      className="navbar bg-base-100"
-      style={{ backgroundColor: "#1C1C1E", position: "fixed" }}
-    >
-      <div className="flex-1">
-        <a href="#about">
+    <nav className="navBar">
+      <div style={{ marginLeft: "2.5vw" }}>
+        <a className="logoAnchor" href="#bio">
           <img
             className="logo"
             src={logo}
@@ -18,75 +15,40 @@ export default function Navbar() {
               width: "50px",
               padding: "3px",
             }}
-          ></img>
+          />
         </a>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <a>
-              <span style={{ color: "teal" }}>01.</span> About
-            </a>
-          </li>
-          <li>
-            <a>
-              <span style={{ color: "teal" }}>02.</span> Projects
-            </a>
-          </li>
-          <li>
-            <a>
-              <span style={{ color: "teal" }}>03.</span> Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    // <nav className="navBar">
-    //   <div style={{ marginLeft: "2.5vw" }}>
-    //     <a className="logoAnchor" href="#bio">
-    // <img
-    //   className="logo"
-    //   src={logo}
-    //   alt="logo"
-    //   style={{
-    //     height: "50px",
-    //     width: "50px",
-    //     padding: "3px",
-    //   }}
-    // />
-    //     </a>
-    //   </div>
 
-    //   <ul>
-    //     <li>
-    //       <a href="#about">
-    // <span style={{ color: "teal" }}>01.</span> About
-    //       </a>
-    //     </li>
-    //     <li>
-    //       <a href="#projects">
-    // <span style={{ color: "teal" }}>02.</span> Projects
-    //       </a>
-    //     </li>
-    //     <li>
-    //       <a href="#contact">
-    // <span style={{ color: "teal" }}>03.</span> Contact
-    //       </a>
-    //     </li>
-    //     <Button
-    //       variant="outlined"
-    //       style={{
-    //         color: "teal",
-    //         borderColor: "teal",
-    //         fontSize: "1vw",
-    //         fontWeight: "800",
-    //         fontFamily: "'Fira Code', monospace",
-    //         border: "2px solid",
-    //       }}
-    //     >
-    //       Resume
-    //     </Button>
-    //   </ul>
-    // </nav>
+      <ul>
+        <li>
+          <a href="#about">
+            <span style={{ color: "teal" }}>01.</span> About
+          </a>
+        </li>
+        <li>
+          <a href="#projects">
+            <span style={{ color: "teal" }}>02.</span> Projects
+          </a>
+        </li>
+        <li>
+          <a href="#contact">
+            <span style={{ color: "teal" }}>03.</span> Contact
+          </a>
+        </li>
+        <Button
+          variant="outlined"
+          style={{
+            color: "teal",
+            borderColor: "teal",
+            fontSize: "1vw",
+            fontWeight: "800",
+            fontFamily: "'Fira Code', monospace",
+            border: "2px solid",
+          }}
+        >
+          Resume
+        </Button>
+      </ul>
+    </nav>
   );
 }
